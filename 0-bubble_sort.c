9b3c23c4ba136b56c817bbe68ead221fcafea0d8
Elijahdanie <elijahdanie@gmail.com>
2021-10-28 14:46:03 +0100

    updated formatting

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,25 +8,25 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    unsigned int i, j, swap, temp;
+unsigned int i, j, swap, temp;
 
-    for (i = 0; i < size; i++)
-    {
-        swap = 0;
-        for ( j = i+1; j < size; j++)
-        {
-            if (array[j] < array[i])
-            {
-                temp = array[j];
-                array[j] = array[i];
-                array[i] = temp;
-                print_array(array, size);
-                swap = 1;
-            }
-        }
-        if (swap == 0)
-        {
-            break;
-        }
-    }
+for (i = 0; i < size; i++)
+{
+swap = 0;
+for ( j = i+1; j < size; j++)
+{
+if (array[j] < array[i])
+{
+temp = array[j];
+array[j] = array[i];
+array[i] = temp;
+print_array(array, size);
+swap = 1;
+}
+}
+if (swap == 0)
+{
+break;
+}
+}
 }
